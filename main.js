4ba79d8da7c2eabb5e5117d3783d6e2ba41c0c09
Евгений <138179155+LapEvgeni@users.noreply.github.com>
2025-03-13 21:18:21 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function dohod_stat(){
+	
+	let bd1 = document.getElementById('bd1');
+	let bd2 = document.getElementById('bd2');
+  	let bd3 = document.getElementById('bd3');
+
+  	bd1.style.display = 'block';
+  	bd2.style.display = 'none';
+  	bd3.style.display = 'none';
+
+}
+
+
+
+function rashod_stat(){
+	
+	let bd1 = document.getElementById('bd1');
+	let bd2 = document.getElementById('bd2');
+  	let bd3 = document.getElementById('bd3');
+
+  	bd1.style.display = 'none';
+  	bd2.style.display = 'none';
+  	bd3.style.display = 'block';
+
+}
